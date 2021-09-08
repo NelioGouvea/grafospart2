cef7368df3fc0fc53315342cda60335395f0e3b8
Ketleen Anne <ketleen@ice.ufjf.br>
2021-09-07 22:20:38 -0300

    adicionado comentario no codigo do guloso

--- Algoritmos/guloso.h
+++ Algoritmos/guloso.h
@@ -21,6 +21,7 @@ using namespace std;
 
 using namespace std;
 
+//função auxiliar para ordenar a lista de arestas do menor peso para o maior
 void ordenaVetorArestas(Grafo *grafo, list<Aresta*> *todasArestas) {
 	map<int, bool> visitado;
 
@@ -58,14 +59,24 @@ void ordenaVetorArestas(Grafo *grafo, list<Aresta*> *todasArestas) {
 Grafo* guloso(Grafo *grafo, int d, int *peso, double *tempo) {
 	clock_t t = clock();
 	
+	//cria- se uma lista de arestas 
 	list<Aresta*> todasArestas;
 
+	//ordena-se a lista pelo peso das arestas (da menor para maior)
 	ordenaVetorArestas(grafo, &todasArestas);
 
+	//cria-se uma arvore sem arestas para cada nó
+	//ou simplificando, um grafo de nós "soltos"( sem aresta )
 	Grafo *s = new Grafo(grafo->getOrdem(), false, true, false);
 
+	//cria-se um contador que representará a quantidade de arestas = vertices-1
 	int cont = 0;
+
+	//enquanto a lista de arestas não for vazia e a quantidade de arestas não ser o limite
 	while(!todasArestas.empty() && cont < grafo->getOrdem() - 1) {
+
+		//seleciona a aresta de menor custo da lista ordenada de arestas
+		// e os par de nó da sua extremidade
 		No *noFonte = s->getNo(todasArestas.front()->getFonteId());
 		No *noAlvo = s->getNo(todasArestas.front()->getAlvoId());
 
@@ -73,17 +84,24 @@ Grafo* guloso(Grafo *grafo, int d, int *peso, double *tempo) {
         
 		Grafo *fecho = fechoDireto(s, noFonte->getId());
     
+		//se os nós da extremidade da arestas não estiverem na mesma subArvore 
+		// e se o grau desses nós forem menor que d (nesse caso, menor que 3)
         if ((fecho->getListaNos().empty() || !fecho->procuraNo(noAlvo->getId())) && noAlvo->getGrau() < d && noFonte->getGrau() < d) {   
             
+			//insere-se a aresta de menor peso entre esses nós (vertices)
             s->inserirAresta(noFonte->getId(), noAlvo->getId(), todasArestas.front()->getPeso());
+
+			//atualiza a quantidade de arestas 
             cont++;
         }
 
+		//remove a primeira aresta de menor peso
 		todasArestas.pop_front();
 	}
 
 	s->criaListaAdjacencia();
 
+	//calcula o peso da arvore, ou seja, o somatorio dos pesos de todas as arestas
 	*peso = calculaPeso(s);
 	
 	t = clock() - t;
